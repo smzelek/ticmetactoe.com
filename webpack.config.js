7c40d2ae1a07a80999f2d0e1d7f1d21732869407
Steven Zelek <szelek@chathamfinancial.com>
2020-06-23 18:21:10 -0400

    test gitlab pages deploy

--- webpack.config.js
+++ webpack.config.js
@@ -2,29 +2,29 @@ const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const HtmlWebpackPluginConfig = new HtmlWebpackPlugin({
-  template: './src/index.html',
-  filename: 'index.html',
-  inject: 'body'
+    template: './src/index.html',
+    filename: 'index.html',
+    inject: 'body'
 });
 
 module.exports = {
-  entry: 
-  {
-    index: './src/index.js',
-    sw: './src/sw.js'
-  },
-  output: {
-    path: path.resolve('dist'),
-    filename: '[name].js'
-  },
-  module: {
-    loaders: [
-      { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
-      { test: /\.css$/, loader: 'style-loader!css-loader' },
-      { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
-      { test: /\.svg$/, loader: 'svg-url-loader' }
-    ]
-  },
-  plugins: [HtmlWebpackPluginConfig]
+    entry:
+    {
+        index: './src/index.js',
+        sw: './src/sw.js'
+    },
+    output: {
+        path: path.resolve('dist'),
+        filename: '[name].js'
+    },
+    module: {
+        loaders: [
+            { test: /\.js$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.jsx$/, loader: 'babel-loader', exclude: /node_modules/ },
+            { test: /\.css$/, loader: 'style-loader!css-loader' },
+            { test: /\.scss$/, loader: 'style-loader!css-loader!sass-loader' },
+            { test: /\.svg$/, loader: 'svg-url-loader' }
+        ]
+    },
+    plugins: [HtmlWebpackPluginConfig]
 }
